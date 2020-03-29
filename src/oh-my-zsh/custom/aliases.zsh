48cb3756cfd8d573c9dc8e39cfca30b74bac79e0
Adam Jackman <adam@acjackman.com>
2020-03-28 18:41:02 -0600

    rearrange aliases

--- src/oh-my-zsh/custom/aliases.zsh
+++ src/oh-my-zsh/custom/aliases.zsh
@@ -1,13 +1,20 @@
-alias zshconfig="subl -n ~/.dotfiles/src/oh-my-zsh/custom ~/.zshrc ~/.zshrc_local"
+# Open/Edit
+alias cdmp="cd $HOME/Dropbox/Dump"
 alias hconfig="subl -n ~/.hammerspoon ~/.hammerspoon/init.lua"
-alias dcmp="docker-compose"
-alias gitsweep="git branch --merged | egrep -v '(^\*|master|dev.*|stg|prod|develop|release/.*)' | xargs git branch -d && git remote | xargs git remote prune && echo 'Branches Remaining: ' && git --no-pager branch | head -n 20"
-alias pyclean="find . -name '*.py[c|o]' -o -name __pycache__ -exec rm -rf {} +"
-alias lspath="tr ':' '\n' <<< \"$PATH\""
-alias pwhich="pyenv which"
-alias flashkb="wally-cli ~/Downloads/$(ls -t ~/Downloads | head -1)"
+alias zshconfig="subl -n ~/.dotfiles/src/oh-my-zsh/custom ~/.zshrc ~/.zshrc_local"
+
+# Command Shortcuts
 alias bi="brew install"
 alias bci="brew cask install"
-alias count_unique='cut -f 1 | sort | uniq -c'
-alias awswhoami="aws sts get-caller-identity"
+alias dcmp="docker-compose"
+alias pwhich="pyenv which"
 
+# Actions
+alias gitsweep="git branch --merged | egrep -v '(^\*|master|dev.*|stg|prod|develop|release/.*)' | xargs git branch -d && git remote | xargs git remote prune && echo 'Branches Remaining: ' && git --no-pager branch | head -n 20"
+alias awswhoami="aws sts get-caller-identity"
+alias pyclean="find . -name '*.py[c|o]' -o -name __pycache__ -exec rm -rf {} +"
+alias flashkb="wally-cli ~/Downloads/$(ls -t ~/Downloads | head -1)"
+alias lspath="tr ':' '\n' <<< \"$PATH\""
+
+# Utilities
+alias count_unique='cut -f 1 | sort | uniq -c'
