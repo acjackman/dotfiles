3af3ba9159ed9b21d257a87d9b8d24a7ad847bb8
Adam Jackman <adam@acjackman.com>
2020-05-10 19:38:05 -0600

    pipx overides pyenv shims

--- src/oh-my-zsh/custom/variables.zsh
+++ src/oh-my-zsh/custom/variables.zsh
@@ -1,5 +1,5 @@
 export EDITOR="/usr/local/bin/subl -n -w"
-export PATH=$PATH:$HOME/.bin:$HOME/.local/bin
+export PATH=$HOME/.local/bin:$PATH:$HOME/.bin
 export WORKON_HOME=$HOME/.virtualenvs
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export PROJECT_HOME=$HOME/Development
