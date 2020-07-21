cc266cbf902703dafebaa73cabd07144f2ee8b55
Adam Jackman <adam@acjackman.com>
2020-07-21 13:23:58 -0600

    add dropbox alias

--- src/oh-my-zsh/custom/aliases.zsh
+++ src/oh-my-zsh/custom/aliases.zsh
@@ -40,5 +40,6 @@ alias ....="cd ../../.. && clear && ls"
 alias .....="cd ../../../.. && clear && ls"
 
 ## Shortcuts to common directories
+alias dbox="pushd ~/Dropbox && clear && ls"
 alias dboxn="pushd ~/Dropbox/Notes && clear && ls -1 | tail -r | grep -v -E '(media|^Z.*|Notes & Settings|.*csv$)' | head -10 && echo '...'"
 alias dev="pushd ~/Development && clear && ls"
