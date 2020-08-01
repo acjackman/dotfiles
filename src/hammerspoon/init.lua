a9f6518d4e829424d573a39f600a65a7cc089ed7
Adam Jackman <adam@acjackman.com>
2020-08-01 15:08:07 -0600

    Re-enable scansnap watcher scan

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -204,17 +204,16 @@ f19 = hs.hotkey.bind({}, 'F19', pressedF19, releasedF19)
 
 -- Launch and quit ScanSnap Manager
 -- usbWatcher = nil
+function usbDeviceCallback(data)
+    if (data["productName"] == "ScanSnap S1300i") then
+        if (data["eventType"] == "added") then
+            hs.application.launchOrFocus("ScanSnap Manager")
+        elseif (data["eventType"] == "removed") then
+            app = hs.appfinder.appFromName("ScanSnap Manager")
+            app:kill()
+        end
+    end
+end
 
--- function usbDeviceCallback(data)
---     if (data["productName"] == "ScanSnap S1300i") then
---         if (data["eventType"] == "added") then
---             hs.application.launchOrFocus("ScanSnap Manager")
---         elseif (data["eventType"] == "removed") then
---             app = hs.appfinder.appFromName("ScanSnap Manager")
---             app:kill()
---         end
---     end
--- end
-
--- usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
--- usbWatcher:start()
+usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
