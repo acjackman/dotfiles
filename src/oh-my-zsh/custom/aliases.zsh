4e709043b14bb821318e61fa3ef2c2e947d28ffb
Adam Jackman <adam@acjackman.com>
2021-01-28 20:28:27 -0700

    make sure alias checks current path

--- src/oh-my-zsh/custom/aliases.zsh
+++ src/oh-my-zsh/custom/aliases.zsh
@@ -26,7 +26,7 @@ alias gitoverwrite="git commit --amend --no-edit && git psf"
 alias awswhoami="aws-cli sts get-caller-identity"
 alias pyclean="find . -name '*.py[c|o]' -o -name __pycache__ -exec rm -rf {} +"
 alias flashkb='wally-cli "$(ls -t ~/Downloads/*.hex | head -1)"'
-alias lspath="tr ':' '\n' <<< \"$PATH\""
+alias lspath='tr ":" "\n" <<< "$PATH"'
 alias sublhost="sudo subl -nw /etc/hosts && sudo killall -HUP mDNSResponder"
 alias sshfingerprint="ssh-keygen -l -E md5 -f"
 
