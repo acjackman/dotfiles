a7345ef629eedaf0e15b91f3afed19eaef6f6484
Adam Jackman <adam@acjackman.com>
2022-02-02 11:41:23 -0700

    poetry should be installed with brew

--- src/oh-my-zsh/custom/plugins/python-poetry/python-poetry.plugin.zsh
+++ src/oh-my-zsh/custom/plugins/python-poetry/python-poetry.plugin.zsh
@@ -5,15 +5,15 @@ else
 fi
 
 
-if [[ $FOUND_PYENV -eq 1 ]]; then
-    if [[ -f "$HOME/.poetry/bin/poetry" ]]; then
-        export PATH="$HOME/.poetry/bin:$PATH"
-        # if [[ -f "$HOME/.zfunc/_poetry" ]]; then
-        # else
-        #     mkdir -p $HOME/.zfunc
-        #     poetry completions zsh > $HOME/.zfunc/_poetry
-        # fi
-        # fpath+=~/.zfunc
-    fi
-fi
+# if [[ $FOUND_PYENV -eq 1 ]]; then
+#     if [[ -f "$HOME/.poetry/bin/poetry" ]]; then
+#         export PATH="$HOME/.poetry/bin:$PATH"
+#         # if [[ -f "$HOME/.zfunc/_poetry" ]]; then
+#         # else
+#         #     mkdir -p $HOME/.zfunc
+#         #     poetry completions zsh > $HOME/.zfunc/_poetry
+#         # fi
+#         # fpath+=~/.zfunc
+#     fi
+# fi
 unset FOUND_POETRY
--- src/oh-my-zsh/custom/variables.zsh
+++ src/oh-my-zsh/custom/variables.zsh
@@ -1,11 +1,14 @@
 export EDITOR="te"
 export PATH=$HOME/.local/bin:$PATH:$HOME/.bin
+
 export WORKON_HOME=$HOME/.virtualenvs
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export PROJECT_HOME=$HOME/Development
+
 export AWS_PAGER=""
 
+export HOMEBREW_NO_AUTO_UPDATE=1
 
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export COOKIECUTTER_CONFIG=$HOME/.dotfiles/src/.cookiecutter.yaml
 
 export TOGGL2HARVEST_CONFIG=$HOME/.toggl2harvest
