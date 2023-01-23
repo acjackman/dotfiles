151ce334180277e8b0c9f580f7b7b3fe32bb2ec2
Adam Jackman <adam@acjackman.com>
2023-01-22 23:31:56 -0800

    hexagons are the bestagons

--- src/doom.d/config.el
+++ src/doom.d/config.el
@@ -395,7 +395,7 @@
 (use-package! hcl-mode)
 
 (setq! org-superstar-special-todo-items nil)
-(setq! org-superstar-headline-bullets-list '("⁕" "⬣" "⁜" "➙" "▷" "▣" "◈"))
+(setq! org-superstar-headline-bullets-list '("⬣" "⁕" "⁜" "➙" "▷" "▣" "◈"))
 
 (after! org
   (set-ligatures! 'org-mode
