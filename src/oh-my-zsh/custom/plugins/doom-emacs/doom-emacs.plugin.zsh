bcc5328b22615598455599977bdf6703237baf41
Adam Jackman <adam@acjackman.com>
2023-06-16 10:59:30 -0700

    move doom dir into .config

--- src/oh-my-zsh/custom/plugins/doom-emacs/doom-emacs.plugin.zsh
+++ src/oh-my-zsh/custom/plugins/doom-emacs/doom-emacs.plugin.zsh
@@ -1,3 +1,7 @@
-if [[ -e "$HOME/.doom.d/" ]]; then
+if [[ -e "$HOME/.emacs.d/bin" ]]; then
   export PATH="$HOME/.emacs.d/bin:$PATH"
+fi
+
+if [[ -e "$HOME/${XDG_CONFIG_HOME:-.config}/emacs/bin" ]]; then
+  export PATH="$HOME/${XDG_CONFIG_HOME:-.config}/emacs/bin:$PATH"
 fi
