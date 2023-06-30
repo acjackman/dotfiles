eeeefff466e4cc7f0fa59448337b7ad203fcbe0d
Adam Jackman <adam@acjackman.com>
2023-06-29 18:15:13 -0700

    setup light theme

--- src/doom/config.el
+++ src/doom/config.el
@@ -25,8 +25,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-tokyo-night)
+;; (setq doom-theme 'doom-one) ;; Dooms's default
+(setq doom-theme 'doom-tokyo-night) ;; Dark mode
+;; (setq doom-theme 'doom-solarized-light) ;; Light mode
 
 ;; Disable most warnings
 (setq warning-minimum-level :emergency)
