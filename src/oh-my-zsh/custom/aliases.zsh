bdf8de3870cb5334af5dfe471dc61db74412c0be
Adam Jackman <adam@acjackman.com>
2023-10-27 14:31:23 -0700

    update aliases

--- src/oh-my-zsh/custom/aliases.zsh
+++ src/oh-my-zsh/custom/aliases.zsh
@@ -21,6 +21,7 @@ alias opl='eval $(op signin)'
 # Actions
 alias o=open
 alias gitsweep="git branch --merged | egrep -v '(^\*|master|dev.*|stg|prod|develop|release/.*)' | xargs git branch -d && git remote | xargs git remote prune && echo 'Branches Remaining: ' && git --no-pager branch | head -n 20"
+alias git-hew-wip="git branch | sed '/^\*/d' | sed '/^\+/d' | xargs git branch -D"
 alias gitoverwrite="git commit --amend --no-edit && git psf"
 alias awswhoami="aws-cli sts get-caller-identity"
 alias pyclean="find . -name '*.py[c|o]' -o -name __pycache__ -exec rm -rf {} +"
@@ -49,13 +50,14 @@ alias pd="popd"
 
 ## Moving up
 alias x="clear && ls"
-alias ..="cd .. && clear && ls"
-alias ...="cd ../.. && clear && ls"
-alias ....="cd ../../.. && clear && ls"
-alias .....="cd ../../../.. && clear && ls"
+alias x.="cd .. && clear && ls"
+alias x..="cd ../.. && clear && ls"
+alias x...="cd ../../.. && clear && ls"
+alias x....="cd ../../../.. && clear && ls"
+alias x.....="cd ../../../../.. && clear && ls"
 
 ## Shortcuts to common directories
 alias dbox="pushd ~/Dropbox && clear && ls"
 alias notes="pushd ~/brain && clear && ls -1 | tail -r | grep -v -E '(media|^Z.*|Notes & Settings|.*csv$)' | head -10 && echo '...'"
-alias dev="pushd ~/Development && clear && echo */ | sed 's:/::g' "
+alias dev="pushd ~/dev && clear && echo */ | sed 's:/::g' "
 alias dwn="pushd ~/Downloads && clear && ls"
