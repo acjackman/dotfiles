9e45b920408da9e615209d10a5ca9bacefb8f064
Adam Jackman <adam@acjackman.com>
2023-12-24 02:16:02 -0800

    cleanup oh-my-zsh

--- src/oh-my-zsh/custom/plugins/doom-emacs/doom-emacs.plugin.zsh
+++
@@ -1,7 +0,0 @@
-if [[ -e "$HOME/.emacs.d/bin" ]]; then
-  export PATH="$HOME/.emacs.d/bin:$PATH"
-fi
-
-if [[ -e "$HOME/${XDG_CONFIG_HOME:-.config}/emacs/bin" ]]; then
-  export PATH="$HOME/${XDG_CONFIG_HOME:-.config}/emacs/bin:$PATH"
-fi
--- src/oh-my-zsh/custom/plugins/hub/hub.plugin.zsh
+++
@@ -1,7 +0,0 @@
-command -v hub &> /dev/null && FOUND_HUB=1 || FOUND_HUB=0
-
-if [[ $FOUND_HUB -eq 1 ]]; then
-    eval "$(hub alias -s)"
-fi
-
-unset FOUND_HUB
