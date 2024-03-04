a678df3ed27eda9797e07292ad409b9815369fcd
Adam Jackman <adam@acjackman.com>
2024-03-03 19:31:05 -0800

    Reconfigure window management dialog

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -128,137 +128,26 @@ elseif hostname == "Ingvar" then
     spoon.WheelOfSeasons:start(os.getenv("HOME") .. "/.config/wallpapers/", 60 * 15, true)
 end
 
--- spoon.RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
-local singleKey = spoon.RecursiveBinder.singleKey
-local keyMap = {
-    [singleKey("d", "wallpapers")] = function()
-        spoon.WheelOfSeasons:shiftWallpapers()
-    end,
-    [singleKey("o", "open+")] = {
-        [singleKey("o", "omnifocus")] = launch_app("OmniFocus"),
-        [singleKey("l", "linear")] = launch_app("Linear"),
-        [singleKey("e", "emacs")] = launch_app("Emacs"),
-        [singleKey("s", "slack")] = launch_app("Slack"),
-        [singleKey("d", "drafts")] = launch_app("Drafts"),
-        [singleKey("f", "finder")] = launch_app("Finder"),
-        [singleKey("=", "calc")] = launch_app("Soulver 3"),
-        [singleKey("t", "terminal")] = launch_app("Alacritty"),
-        [singleKey("b", "browser")] = launch_app("Safari"),
-    },
-    [singleKey("w", "window+")] = {
-        [singleKey("b", "balance")] = run_shell("yabai -m space --balance"),
-        [singleKey("t", "toggle")] = run_shell("yabai -m window --toggle split"),
-        -- [singleKey(',', 'main+1')] = global_binding({"shift", "alt"}, ","),
-        -- [singleKey('.', 'main-1')] = global_binding({"shift", "alt"}, "."),
-        -- [singleKey('h', 'main+1')] = global_binding({"shift", "alt"}, ","),
-        -- [singleKey('l', 'main-1')] = global_binding({"shift", "alt"}, "."),
-        [singleKey("l", "float")] = run_shell("yabai -m window --toggle float"),
-        [singleKey("f", "focus")] = {
-            [singleKey("h", "west")] = run_shell("yabai -m window --focus west"),
-            [singleKey("j", "south")] = run_shell("yabai -m window --focus south"),
-            [singleKey("k", "north")] = run_shell("yabai -m window --focus north"),
-            [singleKey("l", "east")] = run_shell("yabai -m window --focus east"),
-        },
-        [singleKey("m", "move")] = {
-            [singleKey("1", "screen-1")] = run_shell("yabai -m window --display 1; yabai -m display --focus 1"),
-            [singleKey("2", "screen-2")] = run_shell("yabai -m window --display 2; yabai -m display --focus 2"),
-            [singleKey("3", "screen-3")] = run_shell("yabai -m window --display 3; yabai -m display --focus 3"),
-            [singleKey("4", "screen-4")] = run_shell("yabai -m window --display 4; yabai -m display --focus 4"),
-            [singleKey("m", "first")] = run_shell("yabai -m window --warp first"),
-            [singleKey("n", "last")] = run_shell("yabai -m window --warp last"),
-            [singleKey("h", "west")] = run_shell("yabai -m window --warp west"),
-            [singleKey("j", "south")] = run_shell("yabai -m window --warp south"),
-            [singleKey("k", "north")] = run_shell("yabai -m window --warp north"),
-            [singleKey("l", "east")] = run_shell("yabai -m window --warp east"),
-        },
-        [singleKey("n", "new")] = run_shell("yabai -m space --create"),
-    },
-    [singleKey("s", "screen+")] = {
-        [singleKey("f", "focus")] = {
-            [singleKey("1", "screen-1")] = run_shell("yabai -m display --focus 1"),
-            [singleKey("2", "screen-2")] = run_shell("yabai -m display --focus 2"),
-            [singleKey("3", "screen-3")] = run_shell("yabai -m display --focus 3"),
-            [singleKey("4", "screen-4")] = run_shell("yabai -m display --focus 4"),
-            [singleKey("h", "west")] = run_shell("yabai -m display --focus west"),
-            [singleKey("j", "south")] = run_shell("yabai -m display --focus south"),
-            [singleKey("k", "north")] = run_shell("yabai -m display --focus north"),
-            [singleKey("l", "east")] = run_shell("yabai -m display --focus east"),
-        },
-        [singleKey("s", "space")] = {
-            [singleKey("n", "new")] = run_shell("yabai -m space --create"),
-            [singleKey("d", "delete")] = run_shell("yabai -m space --destroy"),
-            [singleKey("h", "previous")] = run_shell("yabai -m space --focus prev || yabai -m space --focus last"),
-            [singleKey("l", "next")] = run_shell("yabai -m space --focus next || yabai -m space --focus first"),
-        },
-        [singleKey("r", "rotate-screen")] = {
-            [singleKey("[", "left")] = rotate_screen_counterclockwise,
-            [singleKey("]", "right")] = rotate_screen_clockwise,
-            [singleKey("0", "0°")] = function()
-                screen:rotate(0)
-            end,
-            [singleKey("1", "90°")] = function()
-                screen:rotate(90)
-            end,
-            [singleKey("2", "180°")] = function()
-                screen:rotate(180)
-            end,
-            [singleKey("3", "270°")] = function()
-                screen:rotate(270)
-            end,
-        },
-        -- [singleKey('d', "wallpapers")] = shift_wallpapers,
-    },
-}
-
-spoon.RecursiveBinder.helperFormat = {
-    -- atScreenEdge = 2,  -- Bottom edge (default value)
-    strokeColor = { white = 0, alpha = 0 },
-    textStyle = { -- An hs.styledtext object
-        font = {
-            name = "JetBrainsMono Nerd Font",
-            size = 18,
-        },
-    },
-}
-
-hs.hotkey.bind({ "ctrl" }, "space", spoon.RecursiveBinder.recursiveBind(keyMap))
-
-hs.hotkey.bind(ctrl_cmd, "h", nil, run_shell("yabai -m window --focus west"))
-hs.hotkey.bind(ctrl_cmd, "j", nil, run_shell("yabai -m window --focus south"))
-hs.hotkey.bind(ctrl_cmd, "k", nil, run_shell("yabai -m window --focus north"))
-hs.hotkey.bind(ctrl_cmd, "l", nil, run_shell("yabai -m window --focus east"))
-hs.hotkey.bind(ctrl_cmd_shift, "h", nil, run_shell("yabai -m display --focus west"))
-hs.hotkey.bind(ctrl_cmd_shift, "j", nil, run_shell("yabai -m display --focus south"))
-hs.hotkey.bind(ctrl_cmd_shift, "k", nil, run_shell("yabai -m display --focus north"))
-hs.hotkey.bind(ctrl_cmd_shift, "l", nil, run_shell("yabai -m display --focus east"))
-
-hs.hotkey.bind(
-    ctrl_cmd_shift,
-    "left",
-    nil,
-    run_shell("yabai -m window west --resize right:-20:0 2> /dev/null || yabai -m window --resize right:-20:0")
-)
-hs.hotkey.bind(
-    ctrl_cmd_shift,
-    "down",
-    nil,
-    run_shell("yabai -m window north --resize bottom:0:20 2> /dev/null || yabai -m window --resize bottom:0:20")
-)
-hs.hotkey.bind(
-    ctrl_cmd_shift,
-    "up",
-    nil,
-    run_shell("yabai -m window south --resize top:0:-20 2> /dev/null || yabai -m window --resize top:0:-20")
-)
-hs.hotkey.bind(
-    ctrl_cmd_shift,
-    "right",
-    nil,
-    run_shell("yabai -m window east --resize left:20:0 2> /dev/null || yabai -m window --resize left:20:0")
-)
-
 -- A global variable for the Hyper Mode
 k = hs.hotkey.modal.new({}, "F18")
+
+-- Enter Hyper Mode when F19 (Hyper/Capslock) is pressed
+pressedF19 = function()
+    k.triggered = false
+    k:enter()
+end
+
+-- Leave Hyper Mode when F19 (Hyper/Capslock) is pressed,
+--   send ESCAPE if no other keys are pressed.
+releasedF19 = function()
+    k:exit()
+    if not k.triggered then
+        hs.eventtap.keyStroke({}, "ESCAPE")
+    end
+end
+
+-- Bind the Hyper key
+f19 = hs.hotkey.bind({}, "F19", pressedF19, releasedF19)
 
 -- Old Application
 old_hyper = {
@@ -393,24 +282,138 @@ end
 
 -- k:bind({}, 'm', nil, function() k.triggered = true; spoon.MuteLight:togglLight() end)
 
--- Enter Hyper Mode when F19 (Hyper/Capslock) is pressed
-pressedF19 = function()
-    k.triggered = false
-    k:enter()
-end
+-- spoon.RecursiveBinder.escapeKey = {{}, 'escape'}  -- Press escape to abort
+local singleKey = spoon.RecursiveBinder.singleKey
+local keyMap = {
+    [singleKey("d", "wallpapers")] = function()
+        spoon.WheelOfSeasons:shiftWallpapers()
+    end,
+    [singleKey("o", "open+")] = {
+        [singleKey("o", "omnifocus")] = launch_app("OmniFocus"),
+        [singleKey("l", "linear")] = launch_app("Linear"),
+        [singleKey("e", "emacs")] = launch_app("Emacs"),
+        [singleKey("s", "slack")] = launch_app("Slack"),
+        [singleKey("d", "drafts")] = launch_app("Drafts"),
+        [singleKey("f", "finder")] = launch_app("Finder"),
+        [singleKey("=", "calc")] = launch_app("Soulver 3"),
+        [singleKey("t", "terminal")] = launch_app("Alacritty"),
+        [singleKey("b", "browser")] = launch_app("Safari"),
+    },
+    [singleKey("w", "window+")] = {
+        [singleKey("b", "balance")] = run_shell("yabai -m space --balance"),
+        [singleKey("t", "toggle")] = run_shell("yabai -m window --toggle split"),
+        -- [singleKey(',', 'main+1')] = global_binding({"shift", "alt"}, ","),
+        -- [singleKey('.', 'main-1')] = global_binding({"shift", "alt"}, "."),
+        -- [singleKey('h', 'main+1')] = global_binding({"shift", "alt"}, ","),
+        -- [singleKey('l', 'main-1')] = global_binding({"shift", "alt"}, "."),
+        [singleKey("l", "float")] = run_shell("yabai -m window --toggle float"),
+        [singleKey("f", "focus")] = {
+            [singleKey("h", "west")] = run_shell("yabai -m window --focus west"),
+            [singleKey("j", "south")] = run_shell("yabai -m window --focus south"),
+            [singleKey("k", "north")] = run_shell("yabai -m window --focus north"),
+            [singleKey("l", "east")] = run_shell("yabai -m window --focus east"),
+        },
+        [singleKey("m", "move")] = {
+            [singleKey("1", "screen-1")] = run_shell("yabai -m window --display 1; yabai -m display --focus 1"),
+            [singleKey("2", "screen-2")] = run_shell("yabai -m window --display 2; yabai -m display --focus 2"),
+            [singleKey("3", "screen-3")] = run_shell("yabai -m window --display 3; yabai -m display --focus 3"),
+            [singleKey("4", "screen-4")] = run_shell("yabai -m window --display 4; yabai -m display --focus 4"),
+            [singleKey("m", "first")] = run_shell("yabai -m window --warp first"),
+            [singleKey("n", "last")] = run_shell("yabai -m window --warp last"),
+            [singleKey("h", "west")] = run_shell("yabai -m window --warp west"),
+            [singleKey("j", "south")] = run_shell("yabai -m window --warp south"),
+            [singleKey("k", "north")] = run_shell("yabai -m window --warp north"),
+            [singleKey("l", "east")] = run_shell("yabai -m window --warp east"),
+        },
+        [singleKey("n", "new")] = run_shell("yabai -m space --create"),
+    },
+    [singleKey("s", "screen+")] = {
+        [singleKey("f", "focus")] = {
+            [singleKey("1", "screen-1")] = run_shell("yabai -m display --focus 1"),
+            [singleKey("2", "screen-2")] = run_shell("yabai -m display --focus 2"),
+            [singleKey("3", "screen-3")] = run_shell("yabai -m display --focus 3"),
+            [singleKey("4", "screen-4")] = run_shell("yabai -m display --focus 4"),
+            [singleKey("h", "west")] = run_shell("yabai -m display --focus west"),
+            [singleKey("j", "south")] = run_shell("yabai -m display --focus south"),
+            [singleKey("k", "north")] = run_shell("yabai -m display --focus north"),
+            [singleKey("l", "east")] = run_shell("yabai -m display --focus east"),
+        },
+        [singleKey("s", "space")] = {
+            [singleKey("n", "new")] = run_shell("yabai -m space --create"),
+            [singleKey("d", "delete")] = run_shell("yabai -m space --destroy"),
+            [singleKey("h", "previous")] = run_shell("yabai -m space --focus prev || yabai -m space --focus last"),
+            [singleKey("l", "next")] = run_shell("yabai -m space --focus next || yabai -m space --focus first"),
+        },
+        [singleKey("r", "rotate-screen")] = {
+            [singleKey("[", "left")] = rotate_screen_counterclockwise,
+            [singleKey("]", "right")] = rotate_screen_clockwise,
+            [singleKey("0", "0°")] = function()
+                screen:rotate(0)
+            end,
+            [singleKey("1", "90°")] = function()
+                screen:rotate(90)
+            end,
+            [singleKey("2", "180°")] = function()
+                screen:rotate(180)
+            end,
+            [singleKey("3", "270°")] = function()
+                screen:rotate(270)
+            end,
+        },
+        -- [singleKey('d', "wallpapers")] = shift_wallpapers,
+    },
+}
 
--- Leave Hyper Mode when F19 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF19 = function()
-    k:exit()
-    if not k.triggered then
-        hs.eventtap.keyStroke({}, "ESCAPE")
-    end
-end
+spoon.RecursiveBinder.helperFormat = {
+    -- atScreenEdge = 2,  -- Bottom edge (default value)
+    strokeColor = { white = 0, alpha = 0 },
+    textStyle = { -- An hs.styledtext object
+        font = {
+            name = "JetBrainsMono Nerd Font",
+            size = 18,
+        },
+    },
+}
 
--- Bind the Hyper key
-f19 = hs.hotkey.bind({}, "F19", pressedF19, releasedF19)
+hs.hotkey.bind(shift_hyper, "space", spoon.RecursiveBinder.recursiveBind(keyMap))
+-- k:bind({}, "space", function()
+--     spoon.RecursiveBinder.recursiveBind(keyMap)
+--     k.triggered = true
+-- end)
 
+hs.hotkey.bind(ctrl_cmd, "h", nil, run_shell("yabai -m window --focus west"))
+hs.hotkey.bind(ctrl_cmd, "j", nil, run_shell("yabai -m window --focus south"))
+hs.hotkey.bind(ctrl_cmd, "k", nil, run_shell("yabai -m window --focus north"))
+hs.hotkey.bind(ctrl_cmd, "l", nil, run_shell("yabai -m window --focus east"))
+hs.hotkey.bind(ctrl_cmd_shift, "h", nil, run_shell("yabai -m display --focus west"))
+hs.hotkey.bind(ctrl_cmd_shift, "j", nil, run_shell("yabai -m display --focus south"))
+hs.hotkey.bind(ctrl_cmd_shift, "k", nil, run_shell("yabai -m display --focus north"))
+hs.hotkey.bind(ctrl_cmd_shift, "l", nil, run_shell("yabai -m display --focus east"))
+
+hs.hotkey.bind(
+    ctrl_cmd_shift,
+    "left",
+    nil,
+    run_shell("yabai -m window west --resize right:-20:0 2> /dev/null || yabai -m window --resize right:-20:0")
+)
+hs.hotkey.bind(
+    ctrl_cmd_shift,
+    "down",
+    nil,
+    run_shell("yabai -m window north --resize bottom:0:20 2> /dev/null || yabai -m window --resize bottom:0:20")
+)
+hs.hotkey.bind(
+    ctrl_cmd_shift,
+    "up",
+    nil,
+    run_shell("yabai -m window south --resize top:0:-20 2> /dev/null || yabai -m window --resize top:0:-20")
+)
+hs.hotkey.bind(
+    ctrl_cmd_shift,
+    "right",
+    nil,
+    run_shell("yabai -m window east --resize left:20:0 2> /dev/null || yabai -m window --resize left:20:0")
+)
 -- Launch and quit ScanSnap Manager
 -- usbWatcher = nil
 -- function usbDeviceCallback(data)
