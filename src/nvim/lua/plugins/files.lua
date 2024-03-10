388b7c0bb6ead0b28bc7aced7d746528cb49a43c
Adam Jackman <adam@acjackman.com>
2024-03-09 22:02:39 -0800

    configure key for nvim oil

--- src/nvim/lua/plugins/files.lua
+++ src/nvim/lua/plugins/files.lua
@@ -1,6 +1,7 @@
 return {
   {
     "stevearc/oil.nvim",
+    keys = { { "-", mode = { "n" }, "<cmd>Oil<cr>", desc = "Open pareent directory" } },
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
