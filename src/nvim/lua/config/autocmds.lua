45d393de3049b93431037510a7e4b59986dd3f10
Adam Jackman <adam@acjackman.com>
2024-03-10 01:14:23 -0800

    Disable format autocommand

--- src/nvim/lua/config/autocmds.lua
+++ src/nvim/lua/config/autocmds.lua
@@ -3,10 +3,10 @@
 -- Add any additional autocmds here
 --
 -- Disable autoformat for lua files
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  -- pattern = { "lua" },
-  pattern = {},
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   -- pattern = { "lua" },
+--   pattern = {},
+--   callback = function()
+--     vim.b.autoformat = false
+--   end,
+-- })
