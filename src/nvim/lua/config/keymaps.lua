5bfaf28765abd77abc2d2675d7a1f5d450cdd879
Adam Jackman <adam@acjackman.com>
2024-03-10 22:34:50 -0700

    Add muscle memory bindings

--- src/nvim/lua/config/keymaps.lua
+++ src/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Simplify Lazy keybind
+vim.keymap.set("n", "<leader>l", "<Nop>")
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+vim.keymap.set("n", "<leader>bs", "<cmd>update<cr>", { desc = "Save buffer" })
+vim.keymap.set("n", "<leader>bS", "<cmd>wa<cr>", { desc = "Save all buffers" })
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Write buffer" })
