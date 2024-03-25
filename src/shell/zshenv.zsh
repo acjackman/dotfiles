458b7566cd35a0ca1e83001649d353ccaf0a10d0
Adam Jackman <adam@acjackman.com>
2024-03-24 19:14:38 -0700

    configure homebrew

--- src/shell/zshenv.zsh
+++ src/shell/zshenv.zsh
@@ -2,3 +2,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
+# Configure Homebrew
+# See https://docs.brew.sh/Manpage#environment
+export HOMEBREW_NO_ENV_HINTS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
