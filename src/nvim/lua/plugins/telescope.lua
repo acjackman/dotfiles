3d4bec7d45e6f6ed34eefea2beac73858f6d456c
Adam Jackman <adam@acjackman.com>
2024-03-27 17:36:37 -0700

    maybe get <leader>. back to browse local files

--- src/nvim/lua/plugins/telescope.lua
+++ src/nvim/lua/plugins/telescope.lua
@@ -8,5 +8,9 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
+    keys = {
+      -- Overload from LazyVim https://github.com/LazyVim/LazyVim/blob/cfbd3582736286433ee5532e1ea3a8d05a1e2649/lua/lazyvim/plugins/editor.lua#L181
+      -- { "<leader>fF", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+    },
   },
 }
