f9fab02efdb7cb22f59db8b5f15eb531cc2c798e
Adam Jackman <adam@acjackman.com>
2024-04-08 10:52:10 -0700

    setup launcher to work on intel

--- src/bin/tmux_launcher.sh
+++ src/bin/tmux_launcher.sh
@@ -2,51 +2,20 @@
 # shellcheck disable=SC2207
 
 # from https://cedaei.com/posts/ideas-from-my-dev-setup-always-tmux/
+#
+export SHELL="/bin/zsh"
 
 # Pickup hoembrew command with no config
-
-if [[ -x "/opt/homebrew/bin/brew" ]]; then
-    PATH="$PATH:/opt/homebrew/bin"
+if [[ -x "/opt/homebrew/bin/tmux" ]]; then
+	export PATH="$PATH:/opt/homebrew/bin"
+	exec /opt/homebrew/bin/tmux new-session -A -D -s main
 fi
 
-# # Doesn't let you press Ctrl-C
-# function ctrl_c() {
-# 	echo -e "\renter nil to drop to normal prompt"
-# }
-#
-# trap ctrl_c SIGINT
-#
-#
-# no_of_terminals=$(tmux list-sessions 2>/dev/null | wc -l)
-# if [[ $no_of_terminals -eq 0 ]]; then
-#     tmux new-session -s "main"
-# else
-#     IFS=$'\n'
-#     output=($(tmux list-sessions))
-#     output_names=($(tmux list-sessions -F\#S))
-#     k=1
-#     echo "Choose the terminal to attach: "
-#     for i in "${output[@]}"; do
-#         echo "$k - $i"
-#         ((k++))
-#     done
-#     echo
-#     echo "Create a new session by entering a name for it"
-#     read -r input
-#     if [[ $input == "" ]]; then
-#         tmux new-session
-#     elif [[ $input == 'nil' ]]; then
-#         # Run the default shell
-#         $SHELL
-#     elif [[ $input =~ ^[0-9]+$ ]] && [[ $input -le $no_of_terminals ]]; then
-#         terminal_name="${output_names[input - 1]}"
-#         tmux attach -t "$terminal_name"
-#     else
-#         tmux new-session -s "$input"
-#     fi
-# fi
+if [[ -x "/usr/local/bin/tmux" ]]; then
+	export PATH="$PATH:/usr/local/bin"
+	exec /usr/local/bin/tmux new-session -A -D -s main
+fi
 
-# exec /bin/zsh -l
-tmux new-session -A -D -s main
-# exec /usr/bin/env zsh
-# exit 0
+# No tmux found, falling back to local shell
+
+exec $SHELL
