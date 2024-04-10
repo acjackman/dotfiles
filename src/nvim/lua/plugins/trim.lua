6992d5bebcf2f6a2c83780fb6e81d2e565d0f421
Adam Jackman <adam@acjackman.com>
2024-04-09 21:52:50 -0700

    setup auto-trim new lines

---
+++ src/nvim/lua/plugins/trim.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "cappyzawa/trim.nvim",
+    -- TODO: make this respect autoformat variable
+    opts = {
+      ft_blocklist = { "markdown" },
+      -- if you want to remove multiple blank lines
+      patterns = {
+        [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+      },
+
+      -- highlight trailing spaces
+      highlight = true,
+    },
+  },
+}
