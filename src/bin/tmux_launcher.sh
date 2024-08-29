cab3a10990cda1f8f5a8564688508901ac37f73c
Adam Jackman <adam@acjackman.com>
2024-08-28 19:31:43 -0700

    launch new windows

--- src/bin/tmux_launcher.sh
+++ src/bin/tmux_launcher.sh
@@ -7,15 +7,20 @@ export SHELL="/bin/zsh"
 
 # Pickup hoembrew command with no config
 if [[ -x "/opt/homebrew/bin/tmux" ]]; then
-	export PATH="$PATH:/opt/homebrew/bin"
-	exec /opt/homebrew/bin/tmux new-session -A -D -s main
+  export PATH="$PATH:/opt/homebrew/bin"
+fi
+if [[ -x "/usr/local/bin/tmux" ]]; then
+  export PATH="$PATH:/usr/local/bin"
 fi
 
-if [[ -x "/usr/local/bin/tmux" ]]; then
-	export PATH="$PATH:/usr/local/bin"
-	exec /usr/local/bin/tmux new-session -A -D -s main
+if [[ -x "/opt/homebrew/bin/tmux" ]] || [[ -x "/usr/local/bin/tmux" ]]; then
+  no_of_terminals=$(tmux list-sessions | wc -l)
+  if [[ "$no_of_terminals" == "0" ]]; then
+    exec tmux new-session -A -D -s main
+  else
+    exec tmux new-session -A -D -s $(($no_of_terminals + 1))
+  fi
 fi
 
 # No tmux found, falling back to local shell
-
 exec $SHELL
