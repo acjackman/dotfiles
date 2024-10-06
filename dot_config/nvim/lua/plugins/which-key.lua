a33645f57c075756584b67d599c315be1b6cd29c
Adam Jackman <adam@acjackman.com>
2024-10-05 20:17:20 -0700

    label notes group

---
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      mode = { "n" },
+      { "<leader>n", group = "notes", icon = { icon = "⬣ ", color = "yellow" } },
+    },
+  },
+}
