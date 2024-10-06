1db9965f309661b9d6bf415d9a53775bc8f0d2f0
Adam Jackman <adam@acjackman.com>
2024-10-06 15:00:30 -0700

    macos stat for kube config

--- dot_kube/run_set_kube_config_permissions.sh
+++ dot_kube/run_set_kube_config_permissions.sh
@@ -2,7 +2,7 @@
 
 FILE="$HOME/.kube/config"
 if [ -f "$FILE" ]; then
-  if [ "$(stat -c %a "$FILE")" != "600" ]; then
+  if [ "$(stat -f "%OLp" "$FILE")" != "600" ]; then
     chmod 600 "$FILE"
   fi
 fi
