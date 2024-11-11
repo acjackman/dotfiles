be4392877099af6cec0e36b935711c3552f18218
Adam Jackman <adam@acjackman.com>
2024-11-10 16:27:05 -0800

    notes

--- dot_config/nvim/lua/plugins/notes.lua
+++ dot_config/nvim/lua/plugins/notes.lua
@@ -30,6 +30,24 @@ return {
         local path = spec.dir / "+" / tostring(spec.id)
         return path:with_suffix(".md")
       end,
+      note_frontmatter_func = function(note)
+        -- Add the title of the note as an alias.
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        local out = { title = note.title, aliases = note.aliases, tags = note.tags }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
       -- see below for full list of options 👇
       attachments = {
         -- The default folder to place images in via `:ObsidianPasteImg`.
