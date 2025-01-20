63594135999765c8ba576206cf1c84cca025885a
Adam Jackman <adam@acjackman.com>
2025-01-19 21:27:18 -0800

    configure sniprun

---
+++ dot_config/nvim/lua/exact_plugins/sniprun.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "michaelb/sniprun",
+    branch = "master",
+
+    build = "sh install.sh",
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+    config = function()
+      require("sniprun").setup({
+        -- your options
+      })
+    end,
+
+    keys = {
+      { "<leader>r", "<cmd>SnipRun<cr>", desc = "Run Code Snippet", mode = { "n", "v" } },
+      { "<leader>R", "<cmd>SnipRunOperator<cr>", desc = "Run Code Snippet", mode = { "n", "v" } },
+    },
+  },
+}
