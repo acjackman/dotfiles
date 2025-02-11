5bafbacae12e30fdb65a255c3d426395d746790b
Adam Jackman <adam@acjackman.com>
2025-02-10 22:49:41 -0800

    lualine with full file path

--- dot_config/nvim/lua/exact_plugins/lualine.lua
+++ dot_config/nvim/lua/exact_plugins/lualine.lua
@@ -1,9 +1,25 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.sections["lualine_z"] = {}
-    end,
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = function(_, opts)
+			local icons = LazyVim.config.icons
+
+			opts.sections["lualine_z"] = {}
+			opts.sections["lualine_c"] = {
+				-- LazyVim.lualine.root_dir(),
+				{
+					"diagnostics",
+					symbols = {
+						error = icons.diagnostics.Error,
+						warn = icons.diagnostics.Warn,
+						info = icons.diagnostics.Info,
+						hint = icons.diagnostics.Hint,
+					},
+				},
+				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+				{ LazyVim.lualine.pretty_path({ relative = "root", length = 0 }) },
+			}
+		end,
+	},
 }
