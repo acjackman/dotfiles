fa6693ade354cc34b86e12ae449e39dd1a8757ac
Adam Jackman <adam@acjackman.com>
2025-04-22 12:08:02 -0700

    enable 1Password ssh bookmarks

--- private_ssh/run_once_enable_1password_ssh.sh
+++ private_ssh/run_once_enable_1password_ssh.sh
@@ -5,5 +5,7 @@ if [ ! -f "$FILE" ]; then
   echo >>$FILE <<EOF
 Host *
   IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+Include ~/.ssh/1Password/config
 EOF
 fi
