dfd97cb4ecea91034bec0bbd9309847d18b93344
Adam Jackman <adam@acjackman.com>
2025-05-08 09:36:58 -0700

    opentofu format

--- dot_config/nvim/lua/exact_plugins/lang-hcl.lua
+++ dot_config/nvim/lua/exact_plugins/lang-hcl.lua
@@ -1,11 +1,4 @@
 return {
-  recommended = function()
-    return LazyVim.extras.wants({
-      ft = { "terraform", "hcl" },
-      root = ".terraform",
-    })
-  end,
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "terraform", "hcl" } },
@@ -24,45 +17,14 @@ return {
     opts = { ensure_installed = { "tflint" } },
   },
   {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        null_ls.builtins.formatting.packer,
-        null_ls.builtins.formatting.opentofu_fmt,
-        -- null_ls.builtins.diagnostics.opentofu_validate, # opentofu_validate not found
-      })
-    end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        -- terraform = { "opentofu_validate" },
-        -- tf = { "opentofu_validate" },
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
-    optional = true,
     opts = {
       formatters_by_ft = {
         hcl = { "packer_fmt" },
-        terraform = { "opentofu_fmt_fmt" },
-        tf = { "opentofu_fmt" },
-        tofu = { "opentofu_fmt" },
-        ["terraform-vars"] = { "opentofu_fmt" },
-      },
-      formatters = {
-        terraform_fmt = {
-          -- Specify the command and its arguments for formatting
-          command = "tofu",
-          args = { "fmt", "-" },
-          stdin = true,
-        },
+        terraform = { "tofu_fmt" },
+        tf = { "tofu_fmt" },
+        tofu = { "tofu_fmt" },
+        ["terraform-vars"] = { "tofu_fmt" },
       },
     },
   },
