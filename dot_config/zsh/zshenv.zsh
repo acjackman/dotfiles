e9dc988851be11005a36f2d05bd7a5f2b9344fba
Adam Jackman <adam@acjackman.com>
2025-08-17 20:53:39 -0700

    switch karabiner elements to goku managed

--- dot_config/zsh/zshenv.zsh
+++ dot_config/zsh/zshenv.zsh
@@ -3,6 +3,8 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 # Configure asdf
-export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-${HOME}/.local/config}/asdf/asdfrc"
+export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
 
-export COOKIECUTTER_CONFIG="${XDG_CONFIG_HOME:-${HOME}/.local/config}/cookiecutter/cookiecutter.yaml"
+export COOKIECUTTER_CONFIG="${XDG_CONFIG_HOME}/cookiecutter/cookiecutter.yaml"
+
+export GOKU_EDN_CONFIG_FILE="${XDG_CONFIG_HOME}/karabiner/karabiner.edn"
