d68120e3f14c0bd5247745bcf2d561e9b23d06ec
Adam Jackman <adam@acjackman.com>
2025-12-30 00:10:25 -0800

    update sketchybar-app-font

--- dot_config/sketchybar/data/run_onchange_icon_map.sh
+++ dot_config/sketchybar/data/run_onchange_icon_map.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-wget -q -O icon_map.json.new https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.32/icon_map.json
+# Font installed with homebrew
+
+wget -q -O icon_map.json.new https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.50/icon_map.json
 if [ -s icon_map.json.new ]; then
   mv icon_map.json.new icon_map.json
 else
