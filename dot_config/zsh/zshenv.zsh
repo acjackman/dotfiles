863c9b5e336f00a15bf262bd4bc84d2502a6ec58
Adam Jackman <adam@acjackman.com>
2026-01-19 12:49:34 -0800

    remove asdf

--- dot_config/zsh/zshenv.zsh
+++ dot_config/zsh/zshenv.zsh
@@ -2,9 +2,6 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
-# Configure asdf
-export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
-
 export COOKIECUTTER_CONFIG="${XDG_CONFIG_HOME}/cookiecutter/cookiecutter.yaml"
 
 export GOKU_EDN_CONFIG_FILE="${XDG_CONFIG_HOME}/karabiner/karabiner.edn"
