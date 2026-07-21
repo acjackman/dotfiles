be0547a5978025a580782e061942fdd909e5cc1a
Adam Jackman <adam@acjackman.com>
2026-07-21 10:35:35 -0700

    sketchybar: bump aerospace plugin to v0.2.1

--- dot_config/sketchybar/plugins/run_onchange_aerospace-plugin.sh
+++ dot_config/sketchybar/plugins/run_onchange_aerospace-plugin.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget -q -O aerospace-plugin https://github.com/acjackman/sketchybar-aerospace-plugin/releases/download/v0.2.0/aerospace-plugin
+wget -q -O aerospace-plugin https://github.com/acjackman/sketchybar-aerospace-plugin/releases/download/v0.2.1/aerospace-plugin
 chmod u+x aerospace-plugin
 if xattr -p com.apple.quarantine aerospace-plugin &>/dev/null; then
   xattr -d com.apple.quarantine aerospace-plugin
